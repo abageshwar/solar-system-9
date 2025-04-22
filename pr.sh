e6dfa67f00686646c1f3da42e90a095904b779ce
Arvind <104875922+abageshwar@users.noreply.github.com>
2025-04-22 12:04:59 -0700

    Update pr.sh

--- pr.sh
+++ pr.sh
@@ -1,7 +1,7 @@
 echo "Opening a Pull Request"
 
 curl -X 'POST' \
-  'http://139.59.21.103:3000/api/v1/repos/siddharth/gitops-argocd/pulls' \
+  'http://github.com/abageshwar/gitops-argocd/pulls' \
   -H 'accept: application/json' \
   -H "authorization: $ARGOCD_TOKEN" \
   -H 'Content-Type: application/json' \
